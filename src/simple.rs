4fdfc96f24939152e0e8a6e1ef2972f9e5c54b85
Filip Lazovic <lazovicff@gmail.com>
2022-05-27 18:03:21 +0200

    global score calculation

--- src/simple.rs
+++ src/simple.rs
@@ -2,28 +2,44 @@ use std::marker::PhantomData;
 
 use halo2_proofs::{
     arithmetic::FieldExt,
-    circuit::{Layouter, Region, SimpleFloorPlanner},
-    plonk::{Advice, Circuit, Column, ConstraintSystem, Error, Instance},
+    circuit::{Layouter, Region, SimpleFloorPlanner, AssignedCell},
+    plonk::{Advice, Circuit, Column, ConstraintSystem, Error, Selector, Instance},
     poly::Rotation,
 };
 
 #[derive(Clone, Debug)]
+struct Opinion<F: FieldExt> {
+	local_score: F,
+	global_score: F,
+}
+
+impl <F: FieldExt> Opinion<F> {
+	fn new(local_score: F, global_score: F) -> Self {
+		Opinion {
+			local_score,
+			global_score,
+		}
+	}
+
+	fn calculate_score(&self) -> F {
+		self.local_score * self.global_score
+	}
+}
+
+#[derive(Clone, Debug)]
 struct FieldConfig<F: FieldExt> {
-    advice1: Column<Advice>,
-    advice2: Column<Advice>,
-    advice3: Column<Advice>,
-    instance: Column<Instance>,
+	advices: [Column<Advice>; 4],
+	instance: Column<Instance>,
+	s_mul: Selector,
     _marker: PhantomData<F>,
 }
 
 #[derive(Default)]
 struct MyCircuit<F: FieldExt> {
-    a: Option<F>,
-    b: Option<F>,
+	opinions: Vec<Option<Opinion<F>>>,
 }
 
 impl<F: FieldExt> Circuit<F> for MyCircuit<F> {
-    // Since we are using a single chip for everything, we can just reuse its config.
     type Config = FieldConfig<F>;
     type FloorPlanner = SimpleFloorPlanner;
 
@@ -32,30 +48,33 @@ impl<F: FieldExt> Circuit<F> for MyCircuit<F> {
     }
 
     fn configure(meta: &mut ConstraintSystem<F>) -> Self::Config {
-        // We create the two advice columns that FieldChip uses for I/O.
         let advice1 = meta.advice_column();
         let advice2 = meta.advice_column();
         let advice3 = meta.advice_column();
+		let advice4 = meta.advice_column();
 
-        // We also need an instance column to store public inputs.
         let instance = meta.instance_column();
 
-		meta.enable_equality(advice3);
+		let s_mul = meta.selector();
+
+		meta.enable_equality(advice1);
+		meta.enable_equality(advice4);
 		meta.enable_equality(instance);
 
-        meta.create_gate("mul", |v_cells| {
-            let lhs = v_cells.query_advice(advice1, Rotation::cur());
-            let rhs = v_cells.query_advice(advice2, Rotation::cur());
-            let out = v_cells.query_advice(advice3, Rotation::cur());
+        meta.create_gate("accumulator", |v_cells| {
+			let sum = v_cells.query_advice(advice1, Rotation::cur());
+            let lhs = v_cells.query_advice(advice2, Rotation::cur());
+            let rhs = v_cells.query_advice(advice3, Rotation::cur());
+            let out = v_cells.query_advice(advice4, Rotation::cur());
+			let s = v_cells.query_selector(s_mul);
 
-            vec![lhs * rhs - out]
+            vec![s * (sum + (lhs * rhs) - out)]
         });
 
         FieldConfig {
-            advice1,
-            advice2,
-            advice3,
-            instance,
+            advices: [advice1, advice2, advice3, advice4],
+			instance,
+			s_mul,
             _marker: PhantomData,
         }
     }
@@ -66,20 +85,37 @@ impl<F: FieldExt> Circuit<F> for MyCircuit<F> {
         mut layouter: impl Layouter<F>,
     ) -> Result<(), Error> {
 
-        let c = layouter.assign_region(|| "mul", |mut region: Region<'_, F>| {
-			let a_cell = region.assign_advice(|| "private", config.advice1, 0, || self.a.ok_or(Error::Synthesis))?;
-			let b_cell = region.assign_advice(|| "private", config.advice2, 0, || self.b.ok_or(Error::Synthesis))?;
-			// Now we can assign the multiplication result, which is to be assigned
-			// into the output position.
-			let value = a_cell.value().and_then(|a| b_cell.value().map(|b| *a * *b));
+        let global_score = layouter.assign_region(|| "accumulator", |mut region: Region<'_, F>| {
+			let mut accumulated_cell: Option<AssignedCell<F, F>> = None;
+			for i in 0..self.opinions.len() {
+				if let Some(opinion) = &self.opinions[i] {
+					// Set selector as enabled
+					config.s_mul.enable(&mut region, i)?;
 
-			// Finally, we do the assignment to the output, returning a
-			// variable to be used in another part of the circuit.
-			region.assign_advice(|| "out", config.advice3, 0, || value.ok_or(Error::Synthesis))
+					let sum_cell = if let Some(accumulated_cell) = accumulated_cell {
+						accumulated_cell.copy_advice(|| "acc", &mut region, config.advices[0], i)?
+					} else {
+						region.assign_advice(|| "acc", config.advices[0], i, || Ok(F::zero()))?
+					};
+
+					let lhs_cell = region.assign_advice(|| "lhs", config.advices[1], i, || Ok(opinion.local_score))?;
+					let rhs_cell = region.assign_advice(|| "rhs", config.advices[2], i, || Ok(opinion.global_score))?;
+
+					let value = lhs_cell.value().and_then(|&a| rhs_cell.value().map(|&b| a * b));
+					let out_score = sum_cell.value().and_then(|&sum| value.map(|v| sum + v));
+
+					let out = region.assign_advice(|| "out", config.advices[3], i, || out_score.ok_or(Error::Synthesis))?;
+
+					accumulated_cell = Some(out);
+				}
+			}
+			accumulated_cell.ok_or(Error::Synthesis)
 		})?;
 
         // Expose the result as a public input to the circuit.
-        layouter.constrain_instance(c.cell(), config.instance, 0)
+		layouter.constrain_instance(global_score.cell(), config.instance, 0)?;
+
+		Ok(())
     }
 }
 
@@ -88,22 +124,19 @@ pub fn main() {
 
     // The number of rows in our circuit cannot exceed 2^k. Since our example
     // circuit is very small, we can pick a very small value here.
-    let k = 4;
+    let k = 8;
 
-    // Prepare the private and public inputs to the circuit!
-    let a = Fp::from(2);
-    let b = Fp::from(3);
-    let c = a * b;
+	let opinion = Opinion::new(Fp::from(2), Fp::from(3));
+	let final_score = opinion.calculate_score().double();
 
     // Instantiate the circuit with the private inputs.
     let circuit = MyCircuit {
-        a: Some(a),
-        b: Some(b),
+		opinions: vec![Some(opinion); 2],
     };
 
     // Arrange the public input. We expose the multiplication result in row 0
     // of the instance column, so we position it there in our public inputs.
-    let mut public_inputs = vec![c];
+    let mut public_inputs = vec![final_score];
 
     // Given the correct public input, our circuit will verify.
     let prover = MockProver::run(k, &circuit, vec![public_inputs.clone()]).unwrap();
@@ -113,4 +146,17 @@ pub fn main() {
     public_inputs[0] += Fp::one();
     let prover = MockProver::run(k, &circuit, vec![public_inputs]).unwrap();
     assert!(prover.verify().is_err());
+
+	    // Create the area you want to draw on.
+    // Use SVGBackend if you want to render to .svg instead.
+    use plotters::prelude::*;
+    let root = BitMapBackend::new("layout.png", (1024, 768)).into_drawing_area();
+    root.fill(&WHITE).unwrap();
+    let root = root
+        .titled("Example Circuit Layout", ("sans-serif", 60))
+        .unwrap();
+
+    halo2_proofs::dev::CircuitLayout::default()
+        .render(5, &circuit, &root)
+        .unwrap();
 }
